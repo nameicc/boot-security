c8f765038806efd2703eb8bc86d75a92fb8373d4
shichuanfeng <shichuanfeng@haier.com>
2021-09-13 11:07:39 +0800

    security自定义表单登录页

--- src/main/java/com/tingyu/security/config/SecurityConfig.java
+++ src/main/java/com/tingyu/security/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.tingyu.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,5 +20,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("tingyu").password("tingyu1234").roles("admin");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/js/**", "/css/**", "/images/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated()
+                .and().formLogin().loginPage("/login.html").permitAll()
+                .and().csrf().disable();
     }
 }
